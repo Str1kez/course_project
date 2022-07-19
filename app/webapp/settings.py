65bb177961d438888877d24dd8e99cb3f6237ade
Str1kez <vazinov@yandex.ru>
2022-07-19 03:21:32 +0300

    added hosts for ssh development

--- app/webapp/settings.py
+++ app/webapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.3", "127.0.0.1"]
 
 
 # Application definition
