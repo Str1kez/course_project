ae4aa0608af1d44aca1aa9ca46c381d7483461e7
Str1kez <vazinov@yandex.ru>
2022-09-26 02:38:29 +0300

    Fixed known problems

--- app/bot/handlers/users/shipping.py
+++ app/bot/handlers/users/shipping.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from aiogram import types
 from aiogram.types import LabeledPrice, Message, ContentType, ShippingOption
 from asgiref.sync import sync_to_async
+from django.db import transaction, DatabaseError
 
 from marketplace.models import Item, TgUser, Order, ShippingOption as ShippingOptionDB
 from bot.loader import dp, bot
@@ -23,6 +24,20 @@ async def get_user_or_create(user_id: int, username: str):
         await sync_to_async(user.save)()
 
     return user
+
+
+def valid_item_amount(item_id: int):
+    try:
+        with transaction.atomic():
+            item = Item.objects.get(id=item_id)
+            if item.amount > 0:
+                item.amount -= 1
+                item.save()
+            else:
+                return False
+    except DatabaseError:
+        return False
+    return True
 
 
 @dp.shipping_query_handler()
@@ -46,14 +61,16 @@ async def shipping(query: types.ShippingQuery):
 @dp.pre_checkout_query_handler()
 async def make_payment(query: types.ShippingQuery):
     # ! Здесь идет бронирование товара
-    item = await sync_to_async(Item.objects.get)(id=int(query.invoice_payload))
     # Если есть товар, иначе отправим ok=False с объяснением
-    # TODO: Возможно здесь нужно реализовать транзакцию на кол-во товара
-    if item.amount > 0:
+    # проходит транзакция с уменьшением кол-ва товара
+
+    is_item_valid = await sync_to_async(valid_item_amount)(int(query.invoice_payload))
+    if is_item_valid:
         await bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True)
     else:
         await bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=False,
-                                            error_message='Данный товар закончился :(')
+                                            error_message='Ошибка в транзакции')
+
     # 1111 1111 1111 1026, 12/22, CVC 000
 
 
@@ -71,7 +88,7 @@ async def check_payment(message: Message):
     shipping_address = message.successful_payment.order_info.shipping_address.as_json() if can_be_shipped else None
     mobile_phone = message.successful_payment.order_info.phone_number if can_be_shipped else None
     receiver_name = message.successful_payment.order_info.name if can_be_shipped else None
-    total_amount = message.successful_payment.total_amount / 100  # TODO: decimal?
+    total_amount = Decimal(message.successful_payment.total_amount) / 100
 
     successful_order = Order(user=user,
                              item=item,
@@ -81,7 +98,5 @@ async def check_payment(message: Message):
                              receiver_name=receiver_name,
                              total_amount=total_amount)
 
-    item.amount -= 1  # TODO: FIX
-    await sync_to_async(item.save)()
     await sync_to_async(successful_order.save)()
     await message.answer(f'Спасибо за покупку\n{item.title}')
